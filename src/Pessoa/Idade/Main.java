e40e9c43bb3bbdec2a1032d1aff6d6ec99f5b3ab
Nicolas <adilsonq1976@gmail.com>
2026-06-19 17:28:25 -0300

    Criando Classe IdadePessoa

--- src/Pessoa/Idade/Main.java
+++ src/Pessoa/Idade/Main.java
@@ -1,4 +1,15 @@
 package Pessoa.Idade;
 
 public class Main {
+    public static void main(String[] args) {
+        IdadePessoa pessoa = new IdadePessoa();
+
+        pessoa.setNome("Nicolas Tamashiro");
+        pessoa.setIdade(18);
+
+        System.out.println("Nome: " + pessoa.getNome());
+        System.out.println("Idade: " + pessoa.getIdade());
+
+        pessoa.verificarIdade();
+    }
 }
