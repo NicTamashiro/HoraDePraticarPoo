81aaeaf1435584ebd31bd7512757141421018a9e
Nicolas <adilsonq1976@gmail.com>
2026-06-19 18:16:35 -0300

    Criando Classe Livro

--- src/Livro/Livro.java
+++ src/Livro/Livro.java
@@ -1,4 +1,32 @@
 package Livro;
 
 public class Livro {
+    private String titulo;
+    private String autor;
+
+    public Livro(String titulo, String autor){
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public void exibirDetalhes(){
+        System.out.println("Titulo: " + titulo);
+        System.out.println("Autor: " + autor);
+    }
 }
--- src/Livro/Main.java
+++ src/Livro/Main.java
@@ -1,4 +1,11 @@
 package Livro;
 
 public class Main {
+    public static void main(String[] args) {
+        Livro livro1 = new Livro("Código Limpo", "Robert C");
+        Livro livro2 = new Livro("Entendendo Algoritmos", "Aditya Bhargava");
+
+        livro1.exibirDetalhes();
+        livro2.exibirDetalhes();
+    }
 }
