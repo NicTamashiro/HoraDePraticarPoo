1a0a5de4fbf7d995deaeb7d2a15fa9721d11f130
Nicolas <adilsonq1976@gmail.com>
2026-06-19 17:40:00 -0300

    Criando Classe Produto

--- src/Produto/Main.java
+++ src/Produto/Main.java
@@ -1,4 +1,13 @@
 package Produto;
 
 public class Main {
+    public static void main(String[] args) {
+        Produto produto1 = new Produto("Celular", 2000.0);
+
+        System.out.println("Produto: " + produto1.getNome());
+        System.out.println("Preco: " + produto1.getPreco());
+
+        produto1.aplicarDesconto(10);
+        System.out.println("Novo preco: " + produto1.getPreco());
+    }
 }
