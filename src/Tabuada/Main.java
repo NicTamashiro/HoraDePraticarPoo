ec64a26248427588c6f343d91912e44ba958709e
Nicolas <adilsonq1976@gmail.com>
2026-06-24 10:24:15 -0300

    Criando Classe TabuadaMultiplicacao com interface Tabuada

---
+++ src/Tabuada/Main.java
@@ -0,0 +1,8 @@
+package Tabuada;
+
+public class Main {
+    public static void main(String[] args) {
+        TabuadaMultiplicacao tabuada = new TabuadaMultiplicacao();
+        tabuada.mostrarTabuada(5);
+    }
+}
---
+++ src/Tabuada/TabuadaMultiplicacao.java
@@ -0,0 +1,11 @@
+package Tabuada;
+
+public class TabuadaMultiplicacao implements Tabuada{
+
+    @Override
+    public void mostrarTabuada(int numero){
+        for(int i = 1; i <= 10; i++){
+            System.out.println(i + " x " + numero + " = " + numero*i);
+        }
+    }
+}
