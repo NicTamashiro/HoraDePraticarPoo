0ffb80ccc35b0ce11340ffdfe308fae9d26e3142
Nicolas <adilsonq1976@gmail.com>
2026-06-25 19:22:03 -0300

    Criando Classe Main

---
+++ src/CalcularArea/Main.java
@@ -0,0 +1,21 @@
+package CalcularArea;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        Circulo circulo = new Circulo();
+        circulo.raio = 5;
+
+        Quadrado quadrado = new Quadrado();
+        quadrado.lado = 4;
+
+        ArrayList<Forma> listaFormas = new ArrayList<>();
+        listaFormas.add(circulo);
+        listaFormas.add(quadrado);
+
+        for(Forma forma : listaFormas){
+            System.out.println("Área: " + forma.calcularArea());
+        }
+    }
+}
