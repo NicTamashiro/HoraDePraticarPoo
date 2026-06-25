8fa9c050f7964fbf922be915f7f3d5ed5e5df326
Nicolas <adilsonq1976@gmail.com>
2026-06-25 14:30:45 -0300

    Criando Classe Pessoa

---
+++ src/TreinandoArrayList/Pessoa.java
@@ -0,0 +1,16 @@
+package TreinandoArrayList;
+
+public class Pessoa {
+    private String nome;
+    private int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: " + this.nome + " | Idade: " + this.idade;
+    }
+}
