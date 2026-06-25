ceaffa170ab1e6d336cb4b06bba45eaae46179f8
Nicolas <adilsonq1976@gmail.com>
2026-06-25 19:37:16 -0300

    Criando Classe ContaBancaria

---
+++ src/Contas/ContaBancaria.java
@@ -0,0 +1,19 @@
+package Contas;
+
+public class ContaBancaria {
+    private int numeroConta;
+    private double saldo;
+
+    public ContaBancaria(int numeroConta, double saldo) {
+        this.numeroConta = numeroConta;
+        this.saldo = saldo;
+    }
+
+    public double getSaldo(){
+        return saldo;
+    }
+
+    public int getNumeroConta(){
+        return numeroConta;
+    }
+}
