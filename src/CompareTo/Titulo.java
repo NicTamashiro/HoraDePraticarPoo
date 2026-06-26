75412e9af0dc398e5b0efb7cb862ceaaf2e58a06
Nicolas <adilsonq1976@gmail.com>
2026-06-26 16:50:33 -0300

    Criando Classe Titulo e usando compareTo

---
+++ src/CompareTo/Titulo.java
@@ -0,0 +1,10 @@
+package CompareTo;
+
+public class Titulo implements Comparable<Titulo>{
+    String nome;
+
+    @Override
+    public int compareTo(Titulo outroTitulo){
+        return this.nome.compareTo(outroTitulo.nome);
+    }
+}
