37df2fb6f1562bb9e95030ccc2cf685650afbf17
Nicolas <adilsonq1976@gmail.com>
2026-06-26 16:55:00 -0300

    Colocando Construtor

--- src/CompareTo/Titulo.java
+++ src/CompareTo/Titulo.java
@@ -3,6 +3,10 @@ package CompareTo;
 public class Titulo implements Comparable<Titulo>{
     String nome;
 
+    public Titulo(String nome) {
+        this.nome = nome;
+    }
+
     @Override
     public int compareTo(Titulo outroTitulo){
         return this.nome.compareTo(outroTitulo.nome);
