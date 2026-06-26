f44527709723bc3d289b82c6a9fa3d524e94ee33
Nicolas <adilsonq1976@gmail.com>
2026-06-26 17:02:22 -0300

    Criando lista polimorfica

--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -6,15 +6,17 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> listaArrayList = new ArrayList<>();
-        listaArrayList.add("Elemento 1");
-        listaArrayList.add("Elemento 2");
+        List<String> listaPolimorfica;
 
-        List<String> listaLinkedList = new LinkedList<>();
-        listaLinkedList.add("Elemento A");
-        listaLinkedList.add("Elemento B");
+        listaPolimorfica = new ArrayList<>();
+        listaPolimorfica.add("Elemento 1");
+        listaPolimorfica.add("Elemento 2");
+        System.out.println("ArrayList: " + listaPolimorfica);
 
-        System.out.println("ArrayList: " + listaArrayList);
-        System.out.println("LinkedList: " + listaLinkedList);
+        listaPolimorfica = new LinkedList<>();
+        listaPolimorfica.add("Elemento A");
+        listaPolimorfica.add("Elemento B");
+        System.out.println("LinkedList: " + listaPolimorfica);
+
     }
 }
