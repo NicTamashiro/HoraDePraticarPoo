7682fb86a0377d9c922e20ce4088c627be7f4385
Nicolas <adilsonq1976@gmail.com>
2026-06-26 16:55:16 -0300

    Ordenando Titulos

---
+++ src/CompareTo/Main.java
@@ -0,0 +1,20 @@
+package CompareTo;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Titulo> listaTitulos = new ArrayList<>();
+        listaTitulos.add(new Titulo("D"));
+        listaTitulos.add(new Titulo("A"));
+        listaTitulos.add(new Titulo("B"));
+
+        Collections.sort(listaTitulos);
+
+        for(Titulo titulo : listaTitulos){
+            System.out.println(titulo.nome);
+        }
+    }
+}
