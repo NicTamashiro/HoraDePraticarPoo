8c6ec2d9bb69093450b53984255e5686e3e99fdc
Nicolas <adilsonq1976@gmail.com>
2026-06-30 16:39:50 -0300

    Usando .setLeinient

--- src/GsonPessoa/ConversaoJsonParaObjeto.java
+++ src/GsonPessoa/ConversaoJsonParaObjeto.java
@@ -1,14 +1,15 @@
 package GsonPessoa;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 record Pessoa(String nome, int idade, String cidade){}
 
 public class ConversaoJsonParaObjeto {
     public static void main(String[] args) {
-        String jsonPessoa = "{\"nome\":\"Rodrigo\",\"idade\":20,\"cidade\":\"Brasília\"}";
+        String jsonPessoa = "{\"nome\":\"Rodrigo\",\"cidade\":\"Brasília\"}";
 
-        Gson gson = new Gson();
+        Gson gson = new GsonBuilder().setLenient().create();
         Pessoa pessoa = gson.fromJson(jsonPessoa, Pessoa.class);
 
         System.out.println("Objeto Pessoa: " + pessoa);
